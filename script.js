505896ce01119df20cc86892a2369efd4e83e95c
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-03 10:01:44 -0400

    Add New Grid button to reset grid and its size
    Asks user to input a grid size, deletes previous grid and creates a new
    grid.

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 const container = document.querySelector("#container");
-const GRID_SIZE = 16;
+let gridSize = 16;
 
-
-for (let i = 0; i < GRID_SIZE; i++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("gridRow");
-    container.appendChild(rowDiv);
-    
-    for (let i = 0; i < GRID_SIZE; i++) {
-        let colDiv = document.createElement("div");
-        colDiv.classList.add("gridCol");
-        colDiv.textContent = ".";
-        rowDiv.appendChild(colDiv);
-    }
-}
+createGrid(gridSize);
 
 document.addEventListener("mouseover", (e) => {
     if (e.target.className === "gridCol") {
         e.target.classList.add("painted");
     }
 });
+
+const button = document.querySelector("button");
+button.addEventListener("click", getGridSize);
+
+
+function getGridSize() {
+    const userInput = prompt("Enter the new grid size (max 100).");    
+    setGridSize(parseInt(userInput));
+}
+
+function setGridSize(size) {
+    if (size !== NaN && size <= 100 && size > 0) {
+        deleteGrid();
+        createGrid(size);
+    } else {
+        alert("The grid has not been reset. Please enter a valid number.");
+    }
+}
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("gridRow");
+    container.appendChild(rowDiv);
+    
+        for (let i = 0; i < size; i++) {
+            let colDiv = document.createElement("div");
+            colDiv.classList.add("gridCol");
+            colDiv.textContent = ".";
+            rowDiv.appendChild(colDiv);
+        }
+    }
+    gridSize = size;
+}
+
+function deleteGrid() {
+    while (container.firstChild) {
+        console.log("ahh");
+        container.removeChild(container.firstChild);
+    }
+}
